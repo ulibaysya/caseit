0978eb6f631d59db23eaab0500a5ca3c8ea57123
ulibaysya <9loveee@gmail.com>
2026-07-18 20:15:06 +0300

    Add http handler for user creation

---
+++ internal/handler/user/create.go
@@ -0,0 +1,80 @@
+package user
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+
+	pkghttp "github.com/ulibaysya/caseit/pkg/http"
+)
+
+type UserService interface {
+	Create(ctx context.Context, name string, imageURL string) (id int64, err error)
+}
+
+type Create struct {
+	logger      *slog.Logger
+	userService UserService
+}
+
+func NewCreate(logger *slog.Logger, userService UserService) Create {
+	return Create{
+		userService: userService,
+		logger:      logger,
+	}
+}
+
+func (h Create) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	parameters := struct {
+		Name     string `json:"name"`
+		ImageURL string `json:"image_url"`
+	}{}
+	err := json.NewDecoder(r.Body).Decode(&parameters)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			h.logger.LogAttrs(ctx, slog.LevelWarn, "got empty body")
+			pkghttp.ErrorJSON(w, "empty body", http.StatusBadRequest)
+		} else if _, ok := err.(*json.SyntaxError); ok {
+			h.logger.LogAttrs(ctx, slog.LevelWarn, "got invalid json", slog.String("error", err.Error()))
+			pkghttp.ErrorJSON(w, "invalid json", http.StatusBadRequest)
+		} else {
+			h.logger.LogAttrs(ctx, slog.LevelError, "decoding request body", slog.String("error", err.Error()))
+			pkghttp.ErrorJSON500(w)
+		}
+		return
+	}
+
+	if parameters.Name == "" {
+		h.logger.LogAttrs(ctx, slog.LevelWarn, "request with empty name")
+		pkghttp.ErrorJSON(w, "empty name", http.StatusBadRequest)
+		return
+	}
+	if parameters.ImageURL == "" {
+		h.logger.LogAttrs(ctx, slog.LevelWarn, "request with empty image_url")
+		pkghttp.ErrorJSON(w, "empty image_url", http.StatusBadRequest)
+		return
+	}
+
+	id, err := h.userService.Create(ctx, parameters.Name, parameters.ImageURL)
+	if err != nil {
+		h.logger.LogAttrs(ctx, slog.LevelError, "creating user", slog.String("error", err.Error()))
+		pkghttp.ErrorJSON500(w)
+		return
+	}
+
+	w.Header().Set("Location", fmt.Sprintf("/users/%d", id))
+
+	w.WriteHeader(http.StatusCreated)
+
+	if err = pkghttp.WriteJSON(w, id); err != nil {
+		h.logger.LogAttrs(ctx, slog.LevelError, "writing response", slog.String("error", err.Error()))
+		pkghttp.ErrorJSON500(w)
+		return
+	}
+}
---
+++ internal/handler/user/create_test.go
@@ -0,0 +1,142 @@
+package user
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+)
+
+var discardLogger = slog.New(slog.DiscardHandler)
+
+type create struct {
+	testName    string
+	requestBody string
+
+	statusCode        int
+	responseBody      string
+	serviceNumOfCalls int
+	serviceError      error
+	name, imageURL    string
+	location          string
+}
+
+func (c create) test(t *testing.T) {
+	service := &mockUserService{
+		error: c.serviceError,
+	}
+	rec := httptest.NewRecorder()
+	// logger := slog.New(slog.NewTextHandler(t.Output(), &slog.HandlerOptions{Level: slog.LevelDebug}))
+	// NewCreateHandler(logger, service).ServeHTTP(rec, httptest.NewRequestWithContext(t.Context(), http.MethodPost, "/users", bytes.NewBufferString(c.requestBody)))
+	NewCreate(discardLogger, service).ServeHTTP(rec, httptest.NewRequestWithContext(t.Context(), http.MethodPost, "/users", bytes.NewBufferString(c.requestBody)))
+	response := rec.Result()
+	defer response.Body.Close() //nolint:errcheck
+
+	if response.StatusCode != c.statusCode {
+		t.Fatalf("unexpected status code: %v; expected status code: %v", response.StatusCode, c.statusCode)
+	}
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatalf("unexpected error when reading all body: %v", err)
+	}
+	if string(responseBody) != c.responseBody {
+		t.Fatalf("unexpected response body: %s; expected response body: %v", responseBody, c.responseBody)
+	}
+
+	if service.callsNumber != c.serviceNumOfCalls {
+		t.Fatalf("service was called unexpected number of times: %v; expected: %v", service.callsNumber, c.serviceNumOfCalls)
+	}
+
+	if c.name != "" && service.name != c.name {
+		t.Fatalf("unexpected name: %v; expected name: %v", service.name, c.name)
+	}
+	if c.imageURL != "" && service.imageURL != c.imageURL {
+		t.Fatalf("unexpected image_url: %v; expected image_url: %v", service.imageURL, c.imageURL)
+	}
+
+	if responseLocation := response.Header.Get("Location"); c.location != "" && responseLocation != c.location {
+		t.Fatalf("unexpected Location: %v; expected Location: %v", responseLocation, c.location)
+	}
+}
+
+// TODO test logs
+func TestCreateHandler(t *testing.T) {
+	tests := []create{
+		{
+			testName:     "BadJSON",
+			requestBody:  `I am not a JSON`,
+			statusCode:   http.StatusBadRequest,
+			responseBody: `"invalid json"`,
+		},
+		{
+			testName:     "EmptyRequestBody",
+			statusCode:   http.StatusBadRequest,
+			responseBody: `"empty body"`,
+		},
+		{
+			testName:          "OmittedName",
+			requestBody:       `{"image_url":"https://s3.caseit.net/avatars/wmh7Gc--QRLD0O18Vu-U9rDDTkre8sZIwrcHcbpDYzs.jpg"}`,
+			statusCode:        http.StatusBadRequest,
+			responseBody:      `"empty name"`,
+			serviceNumOfCalls: 0,
+		},
+		{
+			testName:          "OmittedImageURL",
+			requestBody:       `{"name":"imechkOkoko"}`,
+			statusCode:        http.StatusBadRequest,
+			responseBody:      `"empty image_url"`,
+			serviceNumOfCalls: 0,
+		},
+		{
+			testName:          "ServiceError",
+			requestBody:       `{"name":"UsErNaMe","image_url":"https://s3.caseit.net/avatars/wmh7Gc--QRLD0O18Vu-U9rDDTkre8sZIwrcHcbpDYzs.jpg"}`,
+			statusCode:        http.StatusInternalServerError,
+			responseBody:      `"Internal Server Error"`,
+			serviceError:      fmt.Errorf("some service error"),
+			serviceNumOfCalls: 1,
+		},
+		{
+			testName:          "Normal",
+			requestBody:       `{"name":"UsErNaMe","image_url":"https://s3.caseit.net/avatars/wmh7Gc--QRLD0O18Vu-U9rDDTkre8sZIwrcHcbpDYzs.jpg"}`,
+			statusCode:        http.StatusCreated,
+			responseBody:      "1",
+			serviceNumOfCalls: 1,
+			name:              "UsErNaMe",
+			imageURL:          "https://s3.caseit.net/avatars/wmh7Gc--QRLD0O18Vu-U9rDDTkre8sZIwrcHcbpDYzs.jpg",
+			location:          "/users/1",
+		},
+	}
+
+	for _, i := range tests {
+		t.Run(i.testName, i.test)
+	}
+}
+
+type mockUserService struct {
+	name, imageURL string
+	mu             sync.Mutex
+	error          error
+	callsNumber    int
+}
+
+func (m *mockUserService) Create(ctx context.Context, name string, image string) (id int64, err error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.callsNumber += 1
+
+	if m.error != nil {
+		return 0, m.error
+	}
+
+	m.name = name
+	m.imageURL = image
+
+	return 1, nil
+}
