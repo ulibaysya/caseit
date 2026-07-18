4863b19f1bc39029a06325b56d3eaa5d364ab954
ulibaysya <9loveee@gmail.com>
2026-07-18 21:44:46 +0300

    Add user storage

---
+++ internal/storage/postgres/user/create.go
@@ -0,0 +1,18 @@
+package user
+
+import (
+	"context"
+	"fmt"
+)
+
+func (s Storage) Create(ctx context.Context, name string, imageURL string) (int64, error) {
+	const sql = "INSERT INTO users (name, image_url) VALUES ($1, $2) RETURNING id;"
+
+	var id int64
+	err := s.pool.QueryRow(ctx, sql, name, imageURL).Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("inserting into users: %w", err)
+	}
+
+	return id, nil
+}
---
+++ internal/storage/postgres/user/user.go
@@ -0,0 +1,13 @@
+package user
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Storage struct {
+	pool *pgxpool.Pool
+}
+
+func NewStorage(pool *pgxpool.Pool) Storage {
+	return Storage{pool: pool}
+}
